38a95dd07d6a83676efb6b7d2dedfc7ca5378ee6
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-10-17 20:36:17 +0200

    tercera_activity añadida. Botones Siguiente/Anterior implementados en SegundaActivity.

--- app/src/main/java/com/diego/pmsm/SegundaActivity.kt
+++ app/src/main/java/com/diego/pmsm/SegundaActivity.kt
@@ -1,11 +1,25 @@
 package com.diego.pmsm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        val siguienteActivity: Button = findViewById(R.id.boton_siguiente)
+        siguienteActivity.setOnClickListener() {
+            val intent = Intent(this, TerceraActivity::class.java)
+            startActivity(intent)
+        }
+
+        val anteriorActivity: Button = findViewById(R.id.boton_anterior)
+        anteriorActivity.setOnClickListener() {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
