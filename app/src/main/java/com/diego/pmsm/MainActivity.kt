0fe10ed049044e2bb456682a76dfa77fc119abca
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-10-17 19:55:40 +0200

    Segunda Activity Añadida.

--- app/src/main/java/com/diego/pmsm/MainActivity.kt
+++ app/src/main/java/com/diego/pmsm/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.diego.pmsm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cambioLayoutButton: Button = findViewById(R.id.cambio_Interfaz)
+        cambioLayoutButton.setOnClickListener() {
+            val intent = Intent(this, SegundaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
