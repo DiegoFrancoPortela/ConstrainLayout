cbe4e737c02e627d60e9661a35192d9f4693bfc8
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-10-17 20:56:59 +0200

    Botón Anterior de activity_tercera.xml creado.

--- app/src/main/java/com/diego/pmsm/TerceraActivity.kt
+++ app/src/main/java/com/diego/pmsm/TerceraActivity.kt
@@ -1,11 +1,19 @@
 package com.diego.pmsm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TerceraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tercera)
+
+        val anteriorActivity: Button = findViewById(R.id.boton_anterior3)
+        anteriorActivity.setOnClickListener() {
+            val intent = Intent(this, SegundaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
